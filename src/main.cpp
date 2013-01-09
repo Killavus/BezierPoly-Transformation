39a31ff60a2759046cc8bc083c5727017be43234
Marcin Grzywaczewski <killavus@gmail.com>
2013-01-09 02:23:10 +0100

    Added some tests

--- src/main.cpp
+++ src/main.cpp
@@ -15,24 +15,73 @@
 
 int pascal[PASCAL_SIZE * PASCAL_SIZE];
 
+void print_bezier_form(std::string title, Floating coeffs[], int degree) {
+  std::cout << "Polynomial " << title << " in Bezier form (degree " << degree
+    << ")" << std::endl;
+  std::cout.precision(5);
+  for(int i = 0; i <= degree; ++i)
+    std::cout << "b" << (degree - i) << ": " << coeffs[i] << " ";
+  std::cout << std::endl;
+}
+
+void print_exp_form(std::string title, Floating coeffs[], int degree) {
+  std::cout << "Polynomial " << title << " in expotential form (degree " << degree
+    << ")" << std::endl;
+  std::cout.precision(5);
+  for(int i = 0; i <= degree; ++i)
+    std::cout << "a" << (degree - i) << ": " << coeffs[i] << " ";
+  std::cout << std::endl;
+}
+
+void print_comparison(std::string poly_title, Floating arg, Floating bez_coeffs[],
+                      Floating exp_coeffs[], int degree, Floating real_val) {
+  std::cout << std::endl << "===================================" << std::endl;
+  std::cout << "Comparison of value computing using Horner and de Casteljau"
+            << std::endl;
+
+  print_bezier_form(poly_title, bez_coeffs, degree);
+  print_exp_form(poly_title, exp_coeffs, degree);
+
+  std::cout << "COMPUTING VALUE AT ARGUMENT " << arg << std::endl;
+
+  Floating de_casteljau_result = de_casteljau(bez_coeffs, degree, arg),
+           horner_result = horner(exp_coeffs, degree, arg);
+
+  std::cout.precision(32);
+  std::cout << "Horner scheme: " << horner_result << std::endl;
+  std::cout << "De Casteljau algorithm: " << de_casteljau_result << std::endl;
+  std::cout << "Real value: " << real_val << std::endl;
+  std::cout << "===================================" << std::endl;
+}
+
 int main(void) {
-  std::cout << "Populating the Pascal triangle..." << std::endl;
   pascal_populate(pascal);
 
-  Floating exp_coeffs[3] = { 2.0, 3.0, 2.0 };
-  Floating bez_coeffs[3];
+  // Some examples:
+  Floating exp_coeff_9tsheby[10] = { 256.0, 0.0, -576.0, 0.0, 432.0, 0.0,
+                                    -120.0, 0.0, 9.0, 0.0 },
+           bez_handwritten[3] = { 7.0, 3.5, 2.0 },
+           exp_handwritten[3],
+           bez_coeff_9tsheby[10];
 
-  to_bezier_form(pascal, 2, exp_coeffs, bez_coeffs);
-  for(int i = 0; i < 3; ++i) {
-    std::cout << bez_coeffs[i] << " ";
-  }
-  std::cout << std::endl;
+  to_bezier_form(pascal, 9, exp_coeff_9tsheby, bez_coeff_9tsheby);
+  to_exp_form(pascal, 2, bez_handwritten, exp_handwritten);
 
-  to_exp_form(pascal, 2, bez_coeffs, exp_coeffs);
-  for(int i = 0; i < 3; ++i) {
-    std::cout << exp_coeffs[i] << " ";
-  }
-  std::cout << std::endl;
+  print_comparison("handwritten example", 0.5, bez_handwritten, 
+                   exp_handwritten, 2, 4.00000);
+  print_comparison("handwritten example", 0.75, bez_handwritten,
+                   exp_handwritten, 2, 5.37500);
+  print_comparison("handwritten example", 1.0, bez_handwritten,
+                   exp_handwritten, 2, 7);
+  print_comparison("handwritten example", 0.7823411, bez_handwritten,
+                   exp_handwritten, 2, 5.57113849349842);
+
+  print_comparison("9th Tshebyshew polynomial", 0.13, bez_coeff_9tsheby, 
+                   exp_coeff_9tsheby, 9, 0.922041);
+  print_comparison("9th Tshebyshew polynomial", 0.17, bez_coeff_9tsheby, 
+                   exp_coeff_9tsheby, 9, 0.922041);
+  print_comparison("9th Tshebyshew polynomial", -0.866025, bez_coeff_9tsheby, 
+                   exp_coeff_9tsheby, 9, 0.0);
 
   return 0;
 }
